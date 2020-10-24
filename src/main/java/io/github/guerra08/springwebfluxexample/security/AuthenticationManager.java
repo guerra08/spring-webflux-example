f416c140f6b14cf6b71a8ecc08e51a1b74a6e3f0
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-24 15:20:19 -0300

    Feat: @guerra08
    Working JWT auth (Bearer + Cookie).

--- src/main/java/io/github/guerra08/springwebfluxexample/security/AuthenticationManager.java
+++ src/main/java/io/github/guerra08/springwebfluxexample/security/AuthenticationManager.java
@@ -1,15 +1,21 @@
 package io.github.guerra08.springwebfluxexample.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @Component
 public class AuthenticationManager implements ReactiveAuthenticationManager {
 
-    private JwtProvider jwtProvider;
+    private final JwtProvider jwtProvider;
 
     @Autowired
     public AuthenticationManager(JwtProvider jwtProvider) {
@@ -18,7 +24,15 @@ public class AuthenticationManager implements ReactiveAuthenticationManager {
 
     @Override
     public Mono<Authentication> authenticate(Authentication authentication) {
-        System.out.println(jwtProvider.validateJwt(authentication.getPrincipal().toString()));
-        return null;
+        try {
+            Jws<Claims> claimsJws = jwtProvider.validateJwt(authentication.getCredentials().toString());
+            System.out.println(claimsJws.getBody());
+            return Mono.just(new UsernamePasswordAuthenticationToken(
+                    claimsJws.getBody().getSubject(),
+                    authentication.getCredentials().toString(),
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))));
+        }catch (Exception e){
+            return Mono.empty();
+        }
     }
 }
